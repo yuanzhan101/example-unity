5186cd8abd806977df340111b1b5edfb56985f9a
unknown <yzhan101@ncsu.edu>
2019-04-06 11:34:54 -0400

    adding pickup and fire particle

---
+++ UnityLearning/Assets/Scripts/PickUp.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour
+{
+    public Transform target;
+
+    void Update()
+    {
+
+    }
+    void OnMouseDown()
+    {
+        this.transform.position = target.position;
+        this.transform.parent = GameObject.Find("FPSController").transform;
+        this.transform.parent = GameObject.Find("FirstPersonCharacter").transform;
+    }
+    void OnMouseUp()
+    {
+        this.transform.parent = GameObject.Find("FPSController").transform;
+        this.transform.parent = null; // unperent the GameObject From The Target
+    }
+}
