2526564ea7db44da8bee05312a58c48a04416d15
unknown <yzhan101@ncsu.edu>
2019-04-08 18:48:24 -0400

    add particle animation, collision failed for unknown reason

---
+++ UnityLearning/Assets/Scripts/Warp.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Warp : MonoBehaviour
+{
+
+    // Update is called once per frame
+    public Transform warptarget;
+
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+
+        if (col.gameObject.tag == "warp001")
+        {
+            this.transform.position = warptarget.position;
+        }
+    }
+}
