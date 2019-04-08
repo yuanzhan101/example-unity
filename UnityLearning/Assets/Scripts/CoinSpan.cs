e5ecdd45cacab53d47a630642845bdba3a136bd1
unknown <yzhan101@ncsu.edu>
2019-04-08 17:25:45 -0400

    add coin and fence

---
+++ UnityLearning/Assets/Scripts/CoinSpan.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinSpan : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform target;
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.Rotate(0, 4, 0, Space.World);
+    }
+}
